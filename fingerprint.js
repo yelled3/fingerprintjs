399617c2670339bb6360cd85af219bf7ada39191
Jarid Margolin <jaridmargolin@gmail.com>
2013-12-18 18:18:29 -0800

    Bump version

--- fingerprint.js
+++ fingerprint.js
@@ -1,5 +1,5 @@
 /*
-* fingerprintJS 0.5.1 - Fast browser fingerprint library
+* fingerprintJS 0.5.2 - Fast browser fingerprint library
 * https://github.com/Valve/fingerprintjs
 * Copyright (c) 2013 Valentin Vasilyev (iamvalentin@gmail.com)
 * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
